4367dfe710b7c056550c9a9c44d6ab3ae935f94c
Rishav <rishavraj4273@gmail.com>
2025-07-25 22:59:18 +0530

    feat: initialize db connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
+import {createFeedbackTable, createInterviewTable, createUsersTable} from "../db/schema.js";
 import pg from 'pg';
 const { Pool } = pg;
-
-import {createInterviewTable} from "../models/interview.js";
 
 let pool;
 
@@ -19,7 +18,10 @@ const connectDB = async () => {
     console.log("Connecting to PostgreSQL...");
     
     const client = await pool.connect();
+  
+    await client.query(createUsersTable);
     await client.query(createInterviewTable);
+    await client.query(createFeedbackTable);
     client.release();
     
     console.log("Connected to database");
