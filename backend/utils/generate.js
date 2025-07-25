b3ec5706741e53898c880755c819dae33f6d0f74
Rishav <rishavraj4273@gmail.com>
2025-07-25 22:58:51 +0530

    fis: add env vars

--- backend/utils/generate.js
+++ backend/utils/generate.js
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import {GoogleGenAI} from "@google/genai";
 
-const ai = new GoogleGenAI({apiKey:process.env.GOOGLE_GENAI_API_KEY||"AIzaSyDXR2z5KyzdAnX0ACwUouwUUGFiev8FIpc"});
+const ai = new GoogleGenAI({apiKey:process.env.GOOGLE_GENAI_API_KEY});
 
-export const model = async(jobPos,jobDesc,jobExp,ques=5)=>await ai.models.generateContent({
+export const model = async(prompt)=>await ai.models.generateContent({
   model: "gemini-2.5-flash-lite-preview-06-17",
-  contents: "Give me " + ques + " interview questions along with answers for a " + jobPos + " position based on the following job description/technologies: " + jobDesc + " and the candidate's experience: " + jobExp + "provide the questions and answers in a JSON format with 'question' and 'answer' keys only.",
+  contents: prompt,
 });
