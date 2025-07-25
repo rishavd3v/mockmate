01b7cf0a78f3a359d6a9ad9704b05c0c33470d1b
Rishav <rishavraj4273@gmail.com>
2025-07-25 16:43:31 +0530

    feat: pricing page

--- frontend/src/pages/Upgrade.jsx
+++ frontend/src/pages/Upgrade.jsx
@@ -1,9 +1,7 @@
 import {
     Card,
-    CardAction,
     CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
@@ -12,7 +10,7 @@ import { Button } from "@/components/ui/button.jsx"
 
 export default function Upgrade(){
     return(
-        <div className="flex flex-col items-center justify-center">
+        <main className="flex flex-col items-center justify-center">
             <h1 className="text-2xl font-semibold">Upgrade</h1>
             <p className="text-gray-600 text-sm">Upgrade to premium plan to access unlimited mock interview</p>
 
@@ -22,7 +20,7 @@ export default function Upgrade(){
                 ))}
             </div>
 
-        </div>
+        </main>
     )
 }
 
