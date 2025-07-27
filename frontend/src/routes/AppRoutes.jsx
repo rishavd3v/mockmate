8330b18fd2aee2f7308c3d58871d0ca20fc926e6
Rishav <rishavraj4273@gmail.com>
2025-07-27 23:32:52 +0530

    feat: public route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,18 +7,23 @@ import InterviewDashboard from '../pages/InterviewDashboard';
 import Interview from '@/pages/Interview';
 import Feedback from '@/pages/Feedback';
 import Upgrade from '@/pages/Upgrade';
+import Profile from '@/pages/Profile';
+import Landing from '@/pages/Landing';
+import PublicRoute from '@/components/Auth/PublicRoute';
 
 export default function AppRoutes(){
   return(
     <Routes>
+      <Route path="/" element={<Landing/>}/>
       <Route element={<MainLayout/>}>
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
         <Route path="/feedback/:mock_id" element={<ProtectedRoute><Feedback/></ProtectedRoute>} />
         <Route path="/interview/:mock_id" element={<ProtectedRoute><InterviewDashboard/></ProtectedRoute>} />
         <Route path="/interview/:mock_id/start" element={<ProtectedRoute><Interview/></ProtectedRoute>} />
         <Route path="/upgrade" element={<ProtectedRoute><Upgrade/></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
       </Route>
-      <Route path="/signin" element={<Login />} />
+      <Route path="/signin" element={<PublicRoute><Login/></PublicRoute>} />
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   )
@@ -30,7 +35,7 @@ function NotFound(){
         <h1 className="text-2xl font-bold mb-4">404 - Page Not Found!</h1>
         <p className="text-sm mb-6">Sorry, the page you're looking for doesn't exist.</p>
         <Link to="/dashboard" className="text-sm text-blue-500 underline">
-          Go back to homepage
+          Go back to dashboard
         </Link>
       </div>
     );
