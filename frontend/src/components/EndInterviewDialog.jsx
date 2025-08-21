f143e3e13edac08e1c806fc2ceeed31e4e9ba42f
Rishav <rishavraj4273@gmail.com>
2025-08-21 23:16:10 +0530

    feat: add EndInterviewDialog component

---
+++ frontend/src/components/EndInterviewDialog.jsx
@@ -0,0 +1,35 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button"
+
+export default function EndInterviewDialog({ onSubmit }) {
+    return(
+        <AlertDialog>
+            <AlertDialogTrigger>
+                <Button variant="destructive" className="w-26">End Interview</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+                </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={onSubmit}>End Interview</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
