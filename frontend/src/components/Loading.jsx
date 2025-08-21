773af743908a11c8c6b4350e194e41360d967106
Rishav <rishavraj4273@gmail.com>
2025-08-21 23:41:20 +0530

    feat: custom className prop

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,8 +1,8 @@
 import { LoaderCircle } from "lucide-react";
 
-export default function Loading() {
+export default function Loading({className}) {
     return(
-        <div className="flex items-center justify-center h-[80vh]">
+        <div className={`flex items-center justify-center ${className}`}>
             <div className="space-y-4">
                 <h1 className="text-xl font-bold flex gap-2 items-center">
                     <LoaderCircle className="animate-spin"/>
