d30c637550db5241f2dc16ded97d4acfd542c818
Rishav <rishavraj4273@gmail.com>
2025-08-21 23:10:42 +0530

    feat: add Toaster component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom';
-
 import AppRoutes from './routes/AppRoutes'
+import { Toaster } from 'sonner';
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes/>
+      <Toaster richColors position="top-right"/>
     </BrowserRouter>
   )
 }
