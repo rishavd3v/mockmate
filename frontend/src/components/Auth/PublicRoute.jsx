e852ade9d5e86a88e6cda81c8482a24035f14abc
Rishav <rishavraj4273@gmail.com>
2025-08-21 23:43:45 +0530

    chore: remove log

--- frontend/src/components/Auth/PublicRoute.jsx
+++ frontend/src/components/Auth/PublicRoute.jsx
@@ -2,7 +2,6 @@ import { Navigate } from "react-router-dom";
 
 export default function PublicRoute({children}){
     const savedUser = localStorage.getItem("user");
-    console.log("PublicRoute", savedUser);
     if(savedUser){
         return <Navigate to="/dashboard" replace />;
     }
