1cc423f3047a9fb7b92d6f46f7ca72495983bca8
Rishav <rishavraj4273@gmail.com>
2025-08-27 01:25:23 +0530

    refractor db connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-import {createFeedbackTable, createInterviewTable, createUsersTable} from "../db/schema.js";
 import pg from 'pg';
 const { Pool } = pg;
 
@@ -18,12 +17,7 @@ const connectDB = async () => {
     console.log("Connecting to PostgreSQL...");
     
     const client = await pool.connect();
-  
-    await client.query(createUsersTable);
-    await client.query(createInterviewTable);
-    await client.query(createFeedbackTable);
     client.release();
-    
     console.log("Connected to database");
   } catch (error) {
     console.error('Database connection error:', error);
