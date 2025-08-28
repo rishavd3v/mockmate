30991bf6589b5db9f935736f721457d87a5e6c88
Rishav <rishavraj4273@gmail.com>
2025-08-28 17:41:04 +0530

    refactor: update routing files

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-import chatRoute from './routes/chat.js';
-import mockRoute from './routes/mock.js';
+import generateRoute from './routes/generate.js';
+import interviewRoute from './routes/interview.js';
 import feedbackRoute from './routes/feedback.js';
 import { verifyToken } from './middleware/authMiddleware.js';
 
 app.use(cors({origin: '*'}));
 app.use(express.json());
-app.use('/chat',verifyToken, chatRoute);
-app.use('/mock',verifyToken, mockRoute);
+app.use('/generate',verifyToken, generateRoute);
+app.use('/interview',verifyToken, interviewRoute);
 app.use('/feedback',verifyToken, feedbackRoute);
 
 app.get('/', (req, res) => {
