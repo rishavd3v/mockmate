47bdff49169a0a8541c32587a31017dad7a884a4
Rishav <rishavraj4273@gmail.com>
2025-08-28 17:26:09 +0530

    fix: update model

--- backend/utils/generate.js
+++ backend/utils/generate.js
@@ -6,6 +6,6 @@ import {GoogleGenAI} from "@google/genai";
 const ai = new GoogleGenAI({apiKey:process.env.GOOGLE_GENAI_API_KEY});
 
 export const model = async(prompt)=>await ai.models.generateContent({
-  model: "gemini-2.5-flash-lite-preview-06-17",
+  model: "gemini-2.5-flash",
   contents: prompt,
 });
